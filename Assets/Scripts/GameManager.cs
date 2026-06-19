3b1c0c479a7cb0d50efc3b3b9085ea49d645b35c
haani22 <ghostdemon12343@gmail.com>
2026-06-19 13:09:47 +0530

    Added menu polish, game over stats, camera shake, and itch.io release

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject gameOverPanel;
     public AudioClip gameOverSound;
+    public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI highScoreText;
     private AudioSource audioSource;
     void Awake()
     {
@@ -28,7 +31,10 @@ public class GameManager : MonoBehaviour
             cam.Shake(0.5f, 1f);
         }
         yield return new WaitForSecondsRealtime(0.5f);
-
+        int score = (int)Time.timeSinceLevelLoad;
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        finalScoreText.text = "Score: " + score;
+        highScoreText.text = "High Score: " + highScore;
         Time.timeScale = 0f;
         gameOverPanel.SetActive(true);
     }
