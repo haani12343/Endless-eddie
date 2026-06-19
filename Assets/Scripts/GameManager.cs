45c44196f6d5fe28aff0373226fe8f352b4b4f0d
haani22 <ghostdemon12343@gmail.com>
2026-06-19 12:12:13 +0530

    Completed main menu and game flow

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,27 +2,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
- public static GameManager instance;
- public GameObject gameOverPanel;
- public AudioClip gameOverSound;
-private AudioSource audioSource;
-void Awake()
-{
-instance = this;
- audioSource = GetComponent<AudioSource>();
-}
-public void GameOver()
-{
- if (gameOverSound != null)
- {
-         audioSource.PlayOneShot(gameOverSound);
-     }
-     Time.timeScale = 0f;
+    public static GameManager instance;
+    public GameObject gameOverPanel;
+    public AudioClip gameOverSound;
+    private AudioSource audioSource;
+    void Awake()
+    {
+        instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void GameOver()
+    {
+        if (gameOverSound != null)
+        {
+            audioSource.PlayOneShot(gameOverSound);
+        }
+        Time.timeScale = 0f;
         gameOverPanel.SetActive(true);
     }
- public void RestartGame()
+    public void RestartGame()
     {
-     Time.timeScale = 1f;
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
     }
 }
